8ba0b55b7b50e7f8f5ea3bacf723d2e636b34de4
ninota-zcy <1187358812@qq.com>
2019-10-26 16:32:44 +0800

    界面

--- surface.py
+++ surface.py
@@ -6,10 +6,13 @@ import cv2
 from PIL import Image, ImageTk
 import threading
 import time
-
+from tkinter import messagebox
+import webbrowser
 
 
 class Surface(ttk.Frame):
+
+
 	pic_path = ""
 	viewhigh = 600
 	viewwide = 600
@@ -18,38 +21,57 @@ class Surface(ttk.Frame):
 	thread_run = False
 	camera = None
 	color_transform = {"green":("绿牌","#55FF55"), "yello":("黄牌","#FFFF00"), "blue":("蓝牌","#6666FF")}
-		
+
 	def __init__(self, win):
 		ttk.Frame.__init__(self, win)
 		frame_left = ttk.Frame(self)
 		frame_right1 = ttk.Frame(self)
 		frame_right2 = ttk.Frame(self)
+		frame_center= ttk.Frame(self)
+
 		win.title("车牌识别")
 		win.state("zoomed")
 		self.pack(fill=tk.BOTH, expand=tk.YES, padx="5", pady="5")
 		frame_left.pack(side=LEFT,expand=1,fill=BOTH)
 		frame_right1.pack(side=TOP,expand=1,fill=tk.Y)
 		frame_right2.pack(side=RIGHT,expand=0)
-		ttk.Label(frame_left, text='原图：').pack(anchor="nw") 
-		ttk.Label(frame_right1, text='车牌位置：').grid(column=0, row=0, sticky=tk.W)
-		
-		from_pic_ctl = ttk.Button(frame_right2, text="来自图片", width=20, command=self.from_pic)
-		from_vedio_ctl = ttk.Button(frame_right2, text="来自摄像头", width=20, command=self.from_vedio)
+		frame_center.pack(side=LEFT,expand=1,fill=BOTH)
+
+		Label(frame_center,text="==\n|欢|\n|迎|\n|使|\n|用|\n|车|\n|牌|\n|识|\n|别|\n|系|\n|统|\n==",fg="red",font = 'Helvetica -49 bold').pack(anchor="nw")
+
+		Label(frame_left, text='原图：',font = 'Helvetica -27 bold').pack(anchor="nw")
+		Label(frame_right1, text='车牌位置：',font = 'Helvetica -27 bold').grid(column=0, row=0, sticky=tk.W)
+
+
+
+
+		from_pic_ctl = Button(frame_right2,text="来自图片",font = 'Helvetica -29 bold',bg="grey",fg="yellow",width=20, command=self.from_pic)
+
+		from_vedio_ctl = Button(frame_right2, text="来自摄像头",font = 'Helvetica -29 bold',bg="grey",fg="yellow",width=20, command=self.from_vedio)
+
+		button3=Button(frame_right2,text="api再识别",font = 'Helvetica -29 bold',bg="grey",fg="yellow",width=20,command=self.open)
+
+
+
 		self.image_ctl = ttk.Label(frame_left)
 		self.image_ctl.pack(anchor="nw")
-		
+
 		self.roi_ctl = ttk.Label(frame_right1)
 		self.roi_ctl.grid(column=0, row=1, sticky=tk.W)
-		ttk.Label(frame_right1, text='识别结果：').grid(column=0, row=2, sticky=tk.W)
+		Label(frame_right1, text='识别结果：',font = 'Helvetica -29 bold').grid(column=0, row=2, sticky=tk.W)
 		self.r_ctl = ttk.Label(frame_right1, text="")
 		self.r_ctl.grid(column=0, row=3, sticky=tk.W)
 		self.color_ctl = ttk.Label(frame_right1, text="", width="20")
 		self.color_ctl.grid(column=0, row=4, sticky=tk.W)
 		from_vedio_ctl.pack(anchor="se", pady="5")
 		from_pic_ctl.pack(anchor="se", pady="5")
+		button3.pack(anchor="se",pady="5")
 		self.predictor = predict.CardPredictor()
 		self.predictor.train_svm()
-		
+
+
+
+
 	def get_imgtk(self, img_bgr):
 		img = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
 		im = Image.fromarray(img)
@@ -99,7 +121,10 @@ class Surface(ttk.Frame):
 		self.thread.setDaemon(True)
 		self.thread.start()
 		self.thread_run = True
-		
+	def api(self):
+		messagebox.showinfo("api再识别","内容")
+	def open(self):
+		webbrowser.open("https://cloud.baidu.com/product/ocr")
 	def from_pic(self):
 		self.thread_run = False
 		self.pic_path = askopenfilename(title="选择识别图片", filetypes=[("jpg图片", "*.jpg")])
@@ -109,6 +134,7 @@ class Surface(ttk.Frame):
 			self.image_ctl.configure(image=self.imgtk)
 			r, roi, color = self.predictor.predict(img_bgr)
 			self.show_roi(r, roi, color)
+
 
 	@staticmethod
 	def vedio_thread(self):
@@ -131,11 +157,14 @@ def close_window():
 		surface.thread_run = False
 		surface.thread.join(2.0)
 	win.destroy()
-	
-	
+
+
 if __name__ == '__main__':
 	win=tk.Tk()
-	
+
+
+
+
 	surface = Surface(win)
 	win.protocol('WM_DELETE_WINDOW', close_window)
 	win.mainloop()
